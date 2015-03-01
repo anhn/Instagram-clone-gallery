5aff6959f748426721187fee064676dde05ec86b
anhn <anhnd85@gmail.com>
2015-03-01 14:17:02 +0100

    instagram gallery clone
    clone from a local repo

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	db: process.env.db || 'localhost',
+	clientSecret: process.env.clientSecret || 'e41c895389314c44a75784efb92c30fe',
+	tokenSecret: process.env.tokenSecret || 'bangaitoitenlanhungheoin'
+};
